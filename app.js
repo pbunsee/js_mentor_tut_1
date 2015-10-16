4951377b9245e6aee4c2dc86fc19e047ff9f697a
Pranesha Bunsee <pbunsee@gmail.com>
2015-10-15 22:06:47 -0400

    pseudocoded parsing logic

---
+++ app.js
@@ -0,0 +1,27 @@
+
+function parseParagraph(inVal){
+  parse paragraph
+  getSentences();
+}
+
+function getSentences(){
+  regex on terminator . ? !
+  start-find.?!
+    this is a sentence. add it to the sentence hash.
+
+}
+
+function parseWords(){
+  for each sentence, start-findspace
+    parse words
+    getWords();
+}
+
+function getWords(){
+  regex on terminator space
+  this is a word. add it ot the word hash.
+}
+
+
+
+
